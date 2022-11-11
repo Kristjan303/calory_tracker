8e579c256a423a52764438dbccf85c50e3cd940f
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-11-11 10:42:10 +0200

    Added total calories

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ const ItemCtrl = (function(){
             data.items.push(newItem);
             return newItem
         },
+        getTotalCalories: function (){
+            let total = 0;
+            data.items.forEach(function(item){
+                total = total + item.calories;
+            });
+            data.total = total;
+            console.log(data.total)
+            return data.total
+        },
         logData: function(){
             return data
         }
@@ -38,7 +47,8 @@ const UICtrl = (function(){
         itemList: `#item-list`,
         itemNameInput: `#item-name`,
         itemCaloriesInput: `#item-calories`,
-        addBtn: `.add-btn`
+        addBtn: `.add-btn`,
+        totalCalories: `.total-calories`
     }
     return {
         populateItemList: function (items){
@@ -66,6 +76,9 @@ const UICtrl = (function(){
         clearInput: function (){
             document.querySelector(UISelectors.itemNameInput).value = ``;
             document.querySelector(UISelectors.itemCaloriesInput).value = ``;
+        },
+        showTotalCalories: function (totalCalories){
+            document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
         }
 
     }
@@ -80,6 +93,8 @@ const App = (function (ItemCtrl, UICtrl){
         if (input.name !== `` && input.calories !== ``){
             const newItem = ItemCtrl.addItem(input.name, input.calories)
             UICtrl.addListItem(newItem)
+            const totalCalories = ItemCtrl.getTotalCalories();
+            UICtrl.showTotalCalories(totalCalories);
             UICtrl.clearInput();
         }
         event.preventDefault()
